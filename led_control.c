9e673adf6e0f15532aa391d490f79bb8d17ec75c
Livingston <103526651+Livingston-05@users.noreply.github.com>
2023-05-02 12:40:08 +0530

    Update led_control.c

--- led_control.c
+++ led_control.c
@@ -7,14 +7,14 @@ void on()
 {
 	var1=0xF;
 	//configure it to port P1.4 to P1.7
-	LPC_GPIO1->FIOPIN=var1 << 8;
+	LPC_GPIO1->FIOPIN=var1 << 4;
 	//for(r=0;r<3000;r++);
 }
 
 void off()
 {
 	var1=0x0;
-	LPC_GPIO0->FIOPIN=var1 <<8;
+	LPC_GPIO0->FIOPIN=var1 <<4;
 	//for(r=0;r<3000;r++);
 }
 
